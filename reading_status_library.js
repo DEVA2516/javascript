70e4dd0195731baaef0a9fbab5ba727519cfbd47
Your Name <deva162501@gmail.com>
2022-08-12 00:02:21 +0530

    reading status library in js..

---
+++ reading_status_library.js
@@ -0,0 +1,30 @@
+var library = [
+    {
+        'author': 'Bill Gates',
+        'title': 'The Road Ahead',
+        'readingStatus': true
+    },
+    {
+        'author': 'Steve Jobs',
+        'title': 'Walter Isaacson',
+        'readingStatus': true
+    },
+    {
+        'author': 'Suzanne Collins',
+        'title': 'Mockingjay: The Final Book of The Hunger Games',
+        'readingStatus': false
+    }
+];
+
+let key = 'readingStatus';
+
+
+    library.forEach((v,i)=> {
+        //console.log(v);
+        if(library[i][key] == true)
+            console.log(library[i]);
+    });
+
+
+//else 
+//    console.log("not property...");
