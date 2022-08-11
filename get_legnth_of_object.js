80d3514d6083c53c5cba0d9c011f3f84f20dbec3
Your Name <deva162501@gmail.com>
2022-08-11 22:19:06 +0530

    get length of object in js..

---
+++ get_legnth_of_object.js
@@ -0,0 +1,24 @@
+let student = [{ 
+    'name': "David Rayy", 
+    'sclass': "VI",
+    'rollno': 12 
+},
+{ 
+    'name': "Dave gess", 
+    'sclass': "III",
+    'rollno': 11 
+}
+];
+
+let keys_arrays = 0 ;
+
+student.forEach(v => {
+    keys_arrays += Object.keys(v).length;
+    
+});
+
+//keys = Object.keys(student);
+
+console.log(keys_arrays);
+
+//console.log(keys.length);
