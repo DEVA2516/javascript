1e49ff93c8510441f29727d6014e66eb17608170
Your Name <deva162501@gmail.com>
2022-08-11 23:33:17 +0530

    export key values..

---
+++ export_keyvalue.js
@@ -0,0 +1,18 @@
+let teacher = [
+    {'name': 'Elie'}, 
+    {'name': 'Tim'}, 
+    {'name': 'Matt'}, 
+    {'name': 'Colt'}
+];
+key = 'name';
+let new_arr = [];
+
+    exportkey(teacher,key);
+
+function exportkey(teacher,key) {
+    teacher.forEach((v,i)=> {
+        new_arr.push(teacher[i][key]);
+    });
+}
+
+console.log(new_arr);
