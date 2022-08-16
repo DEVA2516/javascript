fa634445c3c41165739689c785872d9e7e8d78c1
Your Name <deva162501@gmail.com>
2022-08-16 14:38:29 +0530

    async

--- learnyounode/my-first-async-io.js
+++ learnyounode/my-first-async-io.js
@@ -1,12 +1,7 @@
 var fs =  require("fs");
-//let a = 45;
-//console.log(a);
+
 
 fs.readFile(process.argv[2],(err,buf) => { 
     buf = buf.toString().split("\n");
     console.log(buf.length-1);
 } );
-//a = 55;
-//console.log(a);
-
-//fs.readFile()
