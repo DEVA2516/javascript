e2e2417f5c6d7d78049329c4e260bcc3cb8ca3b5
Your Name <deva162501@gmail.com>
2022-08-17 23:18:03 +0530

    print min element in array

---
+++ print_min_element.js
@@ -0,0 +1,20 @@
+let a = [10,2,10,0,1];
+let min = a[0];
+
+for (let i = 0; i < a.length; i++){
+    if (min < a[i])
+        min = a[i];
+}
+
+console.log(min);
+
+console.log("----------------");
+
+min = a[0];
+
+a.forEach(v => {
+    if (min < v)
+        min = v;
+});
+
+console.log(min);
