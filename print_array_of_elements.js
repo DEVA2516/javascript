1ed6756a79032069f2d4bfa6a0ef4dca974c63a4
Your Name <deva162501@gmail.com>
2022-08-17 22:43:09 +0530

    print array elements

---
+++ print_array_of_elements.js
@@ -0,0 +1,11 @@
+let a = [6,8,4,3,2,1];
+
+for (let i = 0; i < a.length; i++ ) {
+    console.log(a[i]);
+}
+
+console.log("-------------");
+
+a.forEach(v => {
+    console.log(v);    
+});
--- secode_largest.js
+++ secode_largest.js
@@ -1,31 +1,34 @@
-arr = [1,2,10,8];
+arr = [1, 2, 10, 8, 46, 45, 4, 6, 2];
 
-let max  = arr[0];
-let index = 0;
+let max = arr[0];
+let index = 0, j = 0;
+let max_array = [];
 
-for(let i=0;i<arr.length;i++){
-    if(arr[i]>max) {
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
         max = arr[i];
         index = i;
+        max_array[j++] = max;
         //console.log(i);
     }
- } 
- 
- arr.splice(index,1);
- 
- //console.log(arr);
- 
- max = arr[0];
+}
+console.log(max_array);
 
- if (arr.length > 1) {
+arr.splice(index, 1);
 
- for(let i=0;i<arr.length;i++){
-    if(arr[i] > max)
-        max = arr[i];
- } 
-       console.log(max);
-        //code here
- }  else 
-            return -1;
- 
- 
+
+//console.log(arr);
+
+max = arr[0];
+
+if (arr.length > 1) {
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    console.log(max);
+    //code here
+} else
+    return -1;
+
