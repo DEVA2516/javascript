1b978b8b3b2d17b5ce24537c0ba81001c521aa43
Your Name <deva162501@gmail.com>
2022-08-17 23:12:04 +0530

    print_max_element_using_function

---
+++ print_max_using_function.js
@@ -0,0 +1,26 @@
+a = [6, 7, 8, 1, 2];
+
+console.log(printArrayMax(a));
+console.log("----------------");
+console.log(printEachMax(a));
+
+
+function printArrayMax(a) {
+    let max = a[0];
+
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] > max)
+            max = a[i];
+    }
+    return max;
+}
+
+function printEachMax(a) {
+    let max = a[0];
+    a.forEach(v => {
+        if (v > max)
+            max = v;
+    });
+    return max;
+
+}
